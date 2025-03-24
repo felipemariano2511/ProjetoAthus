44cb872f40126b2ea0a79fe0107a5f12e1d8b773
Felipe Mariano <felipe.mariano2511@gmail.com>
2025-03-24 15:56:53 -0300

    🎉 Commit da estrutura inicial do projeto

---
+++ src/main/java/br/com/unicuritiba/projetoathus/ProjetoAthusApplication.java
@@ -0,0 +1,13 @@
+package br.com.unicuritiba.projetoathus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAthusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAthusApplication.class, args);
+	}
+
+}
