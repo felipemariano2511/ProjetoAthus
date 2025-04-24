76aee151772e4fa0a80352637b25631600d8591a
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-04-24 15:22:44 -0300

    ♻️ refactor (Exceptions) : Melhorias no retorno de erros

--- src/main/java/br/com/unicuritiba/projetoathus/controllers/UsuarioController.java
+++ src/main/java/br/com/unicuritiba/projetoathus/controllers/UsuarioController.java
@@ -1,7 +1,6 @@
 package br.com.unicuritiba.projetoathus.controllers;
 
 import br.com.unicuritiba.projetoathus.domain.models.Usuario;
-import br.com.unicuritiba.projetoathus.domain.repositories.UsuarioRepository;
 import br.com.unicuritiba.projetoathus.dto.UsuarioDTO;
 import br.com.unicuritiba.projetoathus.application.services.UsuarioService;
 import br.com.unicuritiba.projetoathus.mappers.UsuarioMapper;
@@ -15,9 +14,6 @@ import java.util.List;
 public class UsuarioController {
 
     @Autowired
-    private UsuarioRepository repository;
-
-    @Autowired
     private UsuarioService service;
 
     @Autowired
@@ -25,40 +21,21 @@ public class UsuarioController {
 
     @GetMapping
     public ResponseEntity<List<UsuarioDTO>> getAllUsuarios() {
-        List<UsuarioDTO> usuarios = service.getAllUsuarios()
-                .stream()
-                .map(mapper::toDTO)
-                .toList();
-
-        return usuarios.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(usuarios);
+        return ResponseEntity.ok(service.getAllUsuarios().getBody());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UsuarioDTO> getUsuario(@PathVariable Long id) {
-        return service.getUsuario(id)
-                .map(mapper::toDTO)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<ResponseEntity<?>> getUsuario(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getUsuario(id));
     }
 
     @PutMapping
-    public ResponseEntity<UsuarioDTO> putUsuario(@RequestBody Usuario usuario) {
-        try {
-            Usuario atualizado = service.putUsuario(usuario);
-            return ResponseEntity.ok(mapper.toDTO(atualizado));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).build();
-        }
+    public ResponseEntity<UsuarioDTO> putUsuario(@RequestBody Usuario usuario) throws Exception {
+        return ResponseEntity.ok(mapper.toDTO(service.putUsuario(usuario).getBody()));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
-        return repository.findById(id)
-                .map(usuario -> {
-                    repository.deleteById(id);
-                    return ResponseEntity.ok().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<ResponseEntity<?>> deleteUsuario(@PathVariable Long id) {
+        return ResponseEntity.ok(service.deleteUsuario(id));
     }
 }
