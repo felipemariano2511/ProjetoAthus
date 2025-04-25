500b962ea80a0bef635380670dd7841134b4fd38
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-04-25 15:47:07 -0300

    🔧 chore : Configurações para deploy do projeto no Tomcat

--- src/main/java/br/com/unicuritiba/projetoathus/ProjetoAthusApplication.java
+++ src/main/java/br/com/unicuritiba/projetoathus/ProjetoAthusApplication.java
@@ -2,13 +2,22 @@ package br.com.unicuritiba.projetoathus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class ProjetoAthusApplication {
+public class ProjetoAthusApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoAthusApplication.class, args);
 	}
 
+	@Configuration
+	public static class TomcatConfig extends SpringBootServletInitializer {
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(ProjetoAthusApplication.class);
+		}
+	}
 }
