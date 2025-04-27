1e7b0543a4bf440cb8096c5ec961dfe66949ba19
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-04-27 13:48:49 -0300

    Revert "Seguranca de tokens"

--- src/main/java/br/com/unicuritiba/projetoathus/ProjetoAthusApplication.java
+++ src/main/java/br/com/unicuritiba/projetoathus/ProjetoAthusApplication.java
@@ -2,20 +2,12 @@ package br.com.unicuritiba.projetoathus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class ProjetoAthusApplication extends SpringBootServletInitializer {
+public class ProjetoAthusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoAthusApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ProjetoAthusApplication.class);
-	}
-	
 }
--- src/main/java/br/com/unicuritiba/projetoathus/application/services/ServicosService.java
+++ src/main/java/br/com/unicuritiba/projetoathus/application/services/ServicosService.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 @Service
 public class ServicosService {
+
     @Autowired
     private ServicosRepository repository;
 
