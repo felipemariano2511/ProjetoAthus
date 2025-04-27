471d4545935a3b2b916d9c9c9521fbecda4ea08b
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-04-27 14:04:42 -0300

    🚧 exceptions : Melhorias nas Exceptions

--- src/main/java/br/com/unicuritiba/projetoathus/infrastructure/exceptions/handler/ErrorResponse.java
+++ src/main/java/br/com/unicuritiba/projetoathus/infrastructure/exceptions/handler/ErrorResponse.java
@@ -1,6 +1,5 @@
 package br.com.unicuritiba.projetoathus.infrastructure.exceptions.handler;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,15 +13,17 @@ import java.time.LocalDateTime;
 
 public class ErrorResponse {
 
-    private String message;
-    private LocalDateTime data;
+    private LocalDateTime timestamp;
     private int status;
+    private String error;
+    private String message;
     private String path;
 
-    public ErrorResponse(String message, LocalDateTime data, int status, String path) {
-        this.message = message;
-        this.data = data;
+    public ErrorResponse(int status, String error, String message, String path) {
+        this.timestamp = LocalDateTime.now();
         this.status = status;
+        this.error = error;
+        this.message = message;
         this.path = path;
     }
 }
