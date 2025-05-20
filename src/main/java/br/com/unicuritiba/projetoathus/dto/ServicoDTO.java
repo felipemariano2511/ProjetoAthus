90356e2fa3232d7aaaf02006f29436d08dc8ad33
SrLeal42 <gabriellealjoao@gmail.com>
2025-05-19 23:33:02 -0300

    ✨ feat (ServicoDTO/ServicoMapper): Adicionando o DTO e o Mapper do servico

---
+++ src/main/java/br/com/unicuritiba/projetoathus/dto/ServicoDTO.java
@@ -0,0 +1,9 @@
+package br.com.unicuritiba.projetoathus.dto;
+
+import br.com.unicuritiba.projetoathus.domain.models.Categorias;
+
+public record ServicoDTO (
+    Long id,
+    String nome,
+    Categorias categoria_id
+){}
