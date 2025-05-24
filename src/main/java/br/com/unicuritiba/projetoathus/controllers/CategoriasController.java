dcf55e10ab0e9b6aa057eff71ba5775085808b9f
SrLeal42 <gabriellealjoao@gmail.com>
2025-05-23 22:09:12 -0300

    ♻️ refator : Alterando o Controller e Service das categorias
    Alterando o Controller e Service para utilizar o Mapper e o DTO.

--- src/main/java/br/com/unicuritiba/projetoathus/controllers/CategoriasController.java
+++ src/main/java/br/com/unicuritiba/projetoathus/controllers/CategoriasController.java
@@ -2,6 +2,9 @@ package br.com.unicuritiba.projetoathus.controllers;
 
 import br.com.unicuritiba.projetoathus.application.services.CategoriasService;
 import br.com.unicuritiba.projetoathus.domain.models.Categorias;
+import br.com.unicuritiba.projetoathus.dto.CategoriaDTO;
+import br.com.unicuritiba.projetoathus.mappers.CategoriaMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +18,22 @@ public class CategoriasController {
     @Autowired
     private CategoriasService service;
 
+    @Autowired
+    private CategoriaMapper mapper;
+
     @GetMapping
-    public ResponseEntity<List<Categorias>> getCategorias() {
-        return ResponseEntity.ok(service.getAllCategorias());
+    public ResponseEntity<List<CategoriaDTO>> getCategorias() {
+        return ResponseEntity.ok(service.getAllCategorias().getBody());
     }
 
     @PostMapping
-    public ResponseEntity<Categorias> postCategoria(@RequestBody Categorias categoria) {
-        return ResponseEntity.ok(service.saveCategoria(categoria));
+    public ResponseEntity<CategoriaDTO> postCategoria(@RequestBody Categorias categoria) {
+        return ResponseEntity.ok(mapper.toDTO(service.saveCategoria(categoria)));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Categorias> putCategoria(@PathVariable Long id, @RequestBody Categorias categoria) {
-        return ResponseEntity.ok(service.updateCategoria(id, categoria));
+    public ResponseEntity<CategoriaDTO> putCategoria(@PathVariable Long id, @RequestBody Categorias categoria) {
+        return ResponseEntity.ok(mapper.toDTO(service.updateCategoria(id, categoria)));
     }
 
     @DeleteMapping("/{id}")
