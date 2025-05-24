be83a61612fca03bff6097c9979a333662e08828
SrLeal42 <gabriellealjoao@gmail.com>
2025-05-23 22:27:36 -0300

    ✨ feat (NivelAcesso/getCategoria): Atribuição de nivel de acesso a rotas de categorias
    Neste commit foi feito a atribuição de Nivel de acesso as rotas de Categorias, além da criação da rota getCategoria.

--- src/main/java/br/com/unicuritiba/projetoathus/controllers/CategoriasController.java
+++ src/main/java/br/com/unicuritiba/projetoathus/controllers/CategoriasController.java
@@ -7,6 +7,7 @@ import br.com.unicuritiba.projetoathus.mappers.CategoriaMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,16 +27,24 @@ public class CategoriasController {
         return ResponseEntity.ok(service.getAllCategorias().getBody());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseEntity<?>> getCategoria(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getCategoria(id));
+    }
+
+    @PreAuthorize("principal.nivel >= 2") // Rota acessivel apenas para usuarios com nivel ADMIN ou superior
     @PostMapping
     public ResponseEntity<CategoriaDTO> postCategoria(@RequestBody Categorias categoria) {
         return ResponseEntity.ok(mapper.toDTO(service.saveCategoria(categoria)));
     }
 
+    @PreAuthorize("principal.nivel >= 2") // Rota acessivel apenas para usuarios com nivel ADMIN ou superior
     @PutMapping("/{id}")
     public ResponseEntity<CategoriaDTO> putCategoria(@PathVariable Long id, @RequestBody Categorias categoria) {
         return ResponseEntity.ok(mapper.toDTO(service.updateCategoria(id, categoria)));
     }
 
+    @PreAuthorize("principal.nivel >= 2") // Rota acessivel apenas para usuarios com nivel ADMIN ou superior
     @DeleteMapping("/{id}")
     public ResponseEntity deleteCategoria(@PathVariable long id) {
         service.deleteCategoria(id);
