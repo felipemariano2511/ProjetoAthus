cf67cc0df1b6fe1d331a0fbc8fba307342979603
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-27 15:02:51 -0300

    ♻️ refactor : Refatoração dos controladores de reset senha

--- src/main/java/br/com/unicuritiba/projetoathus/controllers/ResetSenhaController.java
+++ src/main/java/br/com/unicuritiba/projetoathus/controllers/ResetSenhaController.java
@@ -1,31 +1,31 @@
 package br.com.unicuritiba.projetoathus.controllers;
 
 import br.com.unicuritiba.projetoathus.application.services.ResetSenhaService;
-import br.com.unicuritiba.projetoathus.dto.ResetSenhaConfirmarDTO;
-import br.com.unicuritiba.projetoathus.dto.ResetSenhaRequestDTO;
-import lombok.RequiredArgsConstructor;
+import br.com.unicuritiba.projetoathus.dto.ResetSenhaDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@RestController
-@RequestMapping("/auth/reset-senha")
-@RequiredArgsConstructor
+@Controller
+@RequestMapping("/auth/resetsenha/confirmar")
 public class ResetSenhaController {
 
-    private final ResetSenhaService resetSenhaService;
+    @Autowired
+    private ResetSenhaService resetSenhaService;
 
-    @PostMapping("/enviar")
-    public ResponseEntity<?> enviarTokenReset(@Valid @RequestBody ResetSenhaRequestDTO request) {
-        return resetSenhaService.enviarTokenReset(request.email());
+    @GetMapping
+    public String mostrarFormulario() {
+        return "view-reset-senha";
     }
 
-    @PostMapping("/confirmar")
-    public ResponseEntity<?> resetarSenha(@Valid @RequestBody ResetSenhaConfirmarDTO request) {
-        return resetSenhaService.resetarSenha(request.token(), request.novaSenha());
+    @PostMapping
+    public ResponseEntity<?> resetarSenha(@Valid @RequestBody ResetSenhaDTO.ResetSenhaConfirmarDto request,
+                                          @RequestParam("token") String token) {
+
+        return resetSenhaService.resetarSenha(token, request.novaSenha());
     }
+
 }
