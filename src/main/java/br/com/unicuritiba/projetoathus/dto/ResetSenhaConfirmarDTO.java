5d262f3b5265dcd98225c1913dd1e8ba07c9b00e
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-27 15:01:59 -0300

    ♻️ refactor : Refatoração dos DTOs utilizados para reset de senha

--- src/main/java/br/com/unicuritiba/projetoathus/dto/ResetSenhaConfirmarDTO.java
+++
@@ -1,4 +0,0 @@
-package br.com.unicuritiba.projetoathus.dto;
-
-public record ResetSenhaConfirmarDTO(String token, String novaSenha) {
-}
--- src/main/java/br/com/unicuritiba/projetoathus/dto/ResetSenhaRequestDTO.java
+++
@@ -1,3 +0,0 @@
-package br.com.unicuritiba.projetoathus.dto;
-
-public record ResetSenhaRequestDTO(String email) {}
