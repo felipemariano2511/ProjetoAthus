c0d7cd0255852cd338579a98de1e587c8a4fac18
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-27 15:03:26 -0300

    ♻️ refactor : Refatoração do serviço de Resetar senha

--- src/main/java/br/com/unicuritiba/projetoathus/application/services/ResetSenhaService.java
+++ src/main/java/br/com/unicuritiba/projetoathus/application/services/ResetSenhaService.java
@@ -1,40 +1,46 @@
 package br.com.unicuritiba.projetoathus.application.services;
 
-import br.com.unicuritiba.projetoathus.domain.models.Usuario;
 import br.com.unicuritiba.projetoathus.domain.repositories.UsuarioRepository;
 import br.com.unicuritiba.projetoathus.infrastructure.exceptions.NotFoundException;
+import br.com.unicuritiba.projetoathus.infrastructure.exceptions.UnauthorizedException;
 import br.com.unicuritiba.projetoathus.infrastructure.security.ResetSenhaToken;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import br.com.unicuritiba.projetoathus.domain.models.Usuario;
+import org.springframework.http.ResponseEntity;
+import java.time.LocalDateTime;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
-@RequiredArgsConstructor
 public class ResetSenhaService {
 
-    private final UsuarioRepository usuarioRepository;
-    private final TokenService tokenService;
-    private final EmailService emailService;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     private final Map<String, ResetSenhaToken> tokens = new ConcurrentHashMap<>();
 
-    public ResponseEntity<?> enviarTokenReset(String email) {
+    public ResponseEntity<Map> enviarTokenReset(String email) {
         Usuario usuario = usuarioRepository.findByEmail(email)
                 .orElseThrow(() -> new NotFoundException("Usuário não encontrado."));
 
         String token = tokenService.gerarResetToken(usuario.getEmail());
-        Instant expiraEm = Instant.now().plusSeconds(900);
+        LocalDateTime expiraEm = LocalDateTime.now().plusSeconds(900);
 
         ResetSenhaToken resetToken = new ResetSenhaToken(token, email, expiraEm);
         tokens.put(token, resetToken);
-        String urlBase = "http://localhost:8080/auth/reset-senha/confirmar";
+        String urlBase = "http://localhost:8080/auth/resetsenha/confirmar";
         String linkReset = urlBase + "?token=" + token;
 
         Map<String, Object> variaveis = new HashMap<>();
@@ -43,19 +49,20 @@ public class ResetSenhaService {
 
         emailService.enviarEmailComReset(
                 usuario.getEmail(),
-                "Redefinição de senha - Instituto Athus",
+                "Redefinir senha - Instituto Athus",
                 "email-reset-senha",
                 variaveis
         );
 
-        return ResponseEntity.ok("Token de redefinição enviado com sucesso.");
+        return ResponseEntity.ok(Map.of("message", "Solicitação para redefinir a senha enviado com sucesso!" +
+                                                            " Por favor, verifique seu email e siga as etapas."));
     }
 
     public ResponseEntity<?> resetarSenha(String token, String novaSenha) {
         ResetSenhaToken reset = tokens.get(token);
 
         if (reset == null || reset.expirado()) {
-            return ResponseEntity.badRequest().body(Map.of("error", "Token inválido ou expirado"));
+            throw new UnauthorizedException("Token inválido ou expirado.");
         }
 
         Usuario usuario = usuarioRepository.findByEmail(reset.getEmail())
@@ -65,6 +72,6 @@ public class ResetSenhaService {
         usuarioRepository.save(usuario);
         tokens.remove(token);
 
-        return ResponseEntity.ok(Map.of("message", "Senha redefinida com sucesso"));
+        return ResponseEntity.ok(Map.of("message", "Senha redefinida com sucesso."));
     }
 }
