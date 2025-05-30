b05b5347eb8f908e9cc7031650bf7d6c5b6712fc
Vinícius Stencel Canto Silva <stencel.vinicius@gmail.com>
2025-05-30 20:24:30 -0300

    🐛 fix : Corrigindo upload de imagens

--- src/main/java/br/com/unicuritiba/projetoathus/application/services/ImagesUploadService.java
+++ src/main/java/br/com/unicuritiba/projetoathus/application/services/ImagesUploadService.java
@@ -1,5 +1,6 @@
 package br.com.unicuritiba.projetoathus.application.services;
 
+import br.com.unicuritiba.projetoathus.infrastructure.storage.ImageStorageService;
 import br.com.unicuritiba.projetoathus.infrastructure.storage.UserImageStorageService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,10 +9,10 @@ import java.util.List;
 @Service
 public class ImagesUploadService {
 
-    private final UserImageStorageService imageStorageService;
+    private final ImageStorageService imageStorageService;
 
-    public ImagesUploadService(UserImageStorageService userImageStorageService) {
-        this.imageStorageService = userImageStorageService;
+    public ImagesUploadService(ImageStorageService imageStorageService) {
+        this.imageStorageService = imageStorageService;
     }
 
     public void upload(List<MultipartFile> images) {
