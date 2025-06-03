2070ab08762c44760f6e9231ffe0fcb9ac405565
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-06-02 21:34:47 -0300

    🚚 move : Movendo de exceptions/handler -> exceptions

--- src/main/java/br/com/unicuritiba/projetoathus/infrastructure/exceptions/CreatedException.java
+++ src/main/java/br/com/unicuritiba/projetoathus/infrastructure/exceptions/CreatedException.java
@@ -1,4 +1,4 @@
-package br.com.unicuritiba.projetoathus.infrastructure.exceptions.handler;
+package br.com.unicuritiba.projetoathus.infrastructure.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
